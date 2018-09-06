b86e849d2bb24465c079e8696cd4d7f48c1678e2
Ostap <ostap024@gmail.com>
2018-09-06 12:40:46 +0300

    create 4 unit tests for calculator

---
+++ ClassLibrary1/MyCalcLib.Tests/MyCalcTests.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyCalcLib;
+namespace MyCalcLib.Tests
+{
+    [TestClass]
+    public class MyCalcTests
+    {
+        [TestMethod]
+        public void Addition_2and4_6returned()
+        {
+            //arrange
+            double x = 2;
+            double y = 4;
+            double expected = 6;
+
+            //act
+            MyCalc c = new MyCalc();
+            double actual = c.Addition(x, y);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Subtraction_2from4_2returned()
+        {
+            //arrange
+            double x = 4;
+            double y = 2;
+            double expected = 2;
+
+            //act
+            MyCalc c = new MyCalc();
+            double actual = c.Subtraction(x, y);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Multiplication_2and4_8returned()
+        {
+            //arrange
+            double x = 2;
+            double y = 4;
+            double expected = 8;
+
+            //act
+            MyCalc c = new MyCalc();
+            double actual = c.Multiplication(x, y);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Division_4by2_6returned()
+        {
+            //arrange
+            double x = 4;
+            double y = 2;
+            double expected = 2;
+
+            //act
+            MyCalc c = new MyCalc();
+            double actual = c.Division(x, y);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
---
+++ ClassLibrary1/MyCalcLib.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyCalcLib.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyCalcLib.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0ffd2c3c-f26f-4e7b-991c-3076795b62b4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
